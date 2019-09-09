c2f35c03f004ab6b737396e028e699ec2226c88a
ivpsa <ivan_vilugron@hotmail.es>
2019-09-09 18:07:07 -0400

    migraciones modelos

---
+++ app/COM_COMUNA.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class COM_COMUNA extends Model
+{
+    //
+}
---
+++ app/ENC_ENCUESTA.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ENC_ENCUESTA extends Model
+{
+    //
+}
